07857b94d40dbee5a94e3cbaa685527c030ef13a
goldensectionlv <>
2021-08-04 15:59:41 +0300

    remove console log

--- store/isMobile.js
+++ store/isMobile.js
@@ -8,11 +8,8 @@ export const getters = {
 
 export const mutations = {
   isMobileCheck: (state) => {
-    console.log(/Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iPod |iPad|iPhone/i
-      .test(navigator.userAgent))
     state.isMobile = /Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iPod|iPad|iPhone/i
       .test(navigator.userAgent)
-    console.log(state.isMobile)
   }
 }
 
